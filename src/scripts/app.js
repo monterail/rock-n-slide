f94fb7a7dd6d7e7f00cef3babff32efddda00320
Belir <patrykpeas@gmail.com>
2016-11-25 17:29:12 +0100

    Beggining of revealing pattern

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,4 @@
 require('./common.js');
 require('./mobile-menu.js');
 require('./slider.js');
+require('./rock-n-slide.js')
---
+++ src/scripts/rock-n-slide.js
@@ -0,0 +1,35 @@
+var rockNslide = (function(config){
+  // CONFIG
+  const DEFAULT_CONFIG = {
+    speed: 400,
+    lazyLoad: true, // Req waypoints
+    classList: {
+      menuTrigger:       "menu-nav__trigger",
+      menuTriggerActive: "menu-nav__trigger--active",
+      menuSlide:         "slide--menu",
+      menuSlideActive:   "slide--menu-active",
+      menuItem:          "menu__nav-anchor",
+      scrollText:        "slide__scrolling-text"
+    }
+  }
+  // Check dependencies
+
+  // Variables
+
+  // Helpers
+
+  // prevSlide
+
+  // nextSlide
+
+  // showMenu
+
+  // Watchers
+
+  return {
+    scrollToTop: "scrollToTop",
+    prevSlide: "prevSlide",
+    nextSlide: "nextSlide",
+    toggleMenu: "toggleMenu"
+  }
+})();
